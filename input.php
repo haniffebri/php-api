059b9bc4f10150f171c82519dc2c2fa49baf7d3b
haniffebri <haniffebri31@gmail.com>
2018-10-25 00:36:08 +0700

    Edit input data

--- input.php
+++ input.php
@@ -1,17 +1,49 @@
-<?php
-include 'koneksi.php';
-$id = $_POST['id'];
-$username = $_POST['username'];
-$password = $_POST['password'];
-$level = $_POST['level'];
-$fullname = $_POST['fullname'];
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Membuat Dengan PHP dan PHPMyAdmin - Menampilkan data dari database</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="judul">
+		<h1>Membuat Dengan PHP Dan MySQL</h1>
+		<h2>Menampilkan data</h2>
 
-$sql_insert="INSERT INTO profile_class VALUES ('$id','$username','$password','$level', '$fullname')";
+	</div>
 
-mysqli_query($conn,$sql_insert)or die(mysqli_error($conn));
+	<br/>
 
-echo '<script>
-alert("Data Berhasil Disimpan");
-location="index.php?"
-</script>';
-?>
+	<a href="index.php">Lihat Semua Data</a>
+
+	<br/>
+	<h3>Input data baru</h3>
+	<form action="input2.php" method="post">
+		<table>
+      <tr>
+        <td>Id</td>
+        <td><input type="text" name="id"></td>
+      </tr>
+			<tr>
+				<td>Username</td>
+				<td><input type="text" name="username"></td>
+			</tr>
+			<tr>
+				<td>Password</td>
+				<td><input type="text" name="password"></td>
+			</tr>
+			<tr>
+				<td>Level</td>
+				<td><input type="text" name="level"></td>
+			</tr>
+      <tr>
+				<td>Fullname</td>
+				<td><input type="text" name="fullname"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="submit" value="Simpan"></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
